bacd5d8522f3a501578b4702e76d01c48f33f6ee
sanjeet <sanjeetdca1218@gmail.com>
2020-11-15 14:53:26 +0530

    some minor changes

--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -18,8 +18,8 @@ function Slider() {
                         alt="First slide"
                     />
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h3>BPSE PRELIMS</h3>
+                        <p>This test series will bost your preparation</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item interval={500}>
